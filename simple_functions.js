21c4808bf6041cd0ee52e35006207144a4fcff0e
cheetahsweater <aikmanfamily@gmail.com>
2023-10-23 10:40:07 -0500

    Completed exercise 1 (page 1-6)

---
+++ simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings(){
+    console.log("My name is Ana.");
+    console.log("My favorite movie is Perfect Blue.");
+    console.log("My favorite band is They Might Be Giants.");
+};
+
+function whyImHere(){
+    console.log("I joined YearUp and Pluralsight so I could learn more programming and start a career in IT.");
+};
+
+function favoritePlace(){
+    console.log("I love to go to the Richardson Public Library, it is like my happy place and I am always so diligent and productive and comfortable there.");
+};
+
+console.log(favoriteThings());
+console.log(favoritePlace());
+console.log(whyImHere());
